41c4c6862310073bde9b58fc4c33b55dc205e528
your_username <your_email_address@example.com>
2022-12-16 11:45:46 +0700

    update new code

--- test/bai6.js
+++ test/bai6.js
@@ -1,7 +1,7 @@
 // Bài 6 : Cho biến var Text=  “Folder Lock uses a master password to control locked content”.
 // a .  Tìm chuỗi ‘nodemy’ có trong đoạn text trên không ? Xuất hiện ở vị trí bao nhiêu ?.
 // b . Thay thế tất cả các ký tự “a” trong biến Text thanh “@”?
-// c . Lấy 3 ký tự cuối cùng của chuỗi
+// c . Lấy 3 ký tự cuối cùng của chuỗi ?
 function timkiem(Text) {
   //Code here
   //...
